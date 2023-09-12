37499f0bf9fe66f5580ba589aa67665a32b22954
igorcooli <igorcooli@gmail.com>
2023-09-12 19:24:46 -0300

    Innitial commit

---
+++ src/main/java/com/io/financio/FinancioApplication.java
@@ -0,0 +1,13 @@
+package com.io.financio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/io/financio/FinancioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.financio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
