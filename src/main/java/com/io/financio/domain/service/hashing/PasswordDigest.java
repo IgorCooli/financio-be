6695960d2ee2c031984ac68d3e8c9e20c839c131
igorcooli <igorcooli@gmail.com>
2023-09-14 21:27:02 -0300

    [TASK-0001] : Creating password hashing

--- src/main/java/com/io/financio/domain/service/hashing/PasswordDigest.java
+++ src/main/java/com/io/financio/domain/service/hashing/PasswordDigest.java
@@ -1,5 +1,6 @@
 package com.io.financio.domain.service.hashing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
@@ -9,12 +10,17 @@ import java.security.NoSuchAlgorithmException;
 @Service
 public class PasswordDigest {
 
-    public  final int HEXADECIMAL = 16;
-    public  final int POSITIVE_NUMBER = 1;
+    private  final int HEXADECIMAL = 16;
+    private  final int POSITIVE_NUMBER = 1;
+    private final String digestInstance;
+
+    public PasswordDigest(@Value("${crypt.sha256.digestInstance}") String digestInstance) {
+        this.digestInstance = digestInstance;
+    }
 
     public String execute(String password) throws NoSuchAlgorithmException {
         var digestedMessage = MessageDigest
-                .getInstance("SHA-256")
+                .getInstance(digestInstance)
                 .digest(password.getBytes());
 
         return new BigInteger(POSITIVE_NUMBER, digestedMessage).toString(HEXADECIMAL);
