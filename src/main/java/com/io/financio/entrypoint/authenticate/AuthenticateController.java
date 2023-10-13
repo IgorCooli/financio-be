bbf663db50e4b2df53a93ff219bb4ca31d690487
igorcooli <igorcooli@gmail.com>
2023-10-13 00:13:22 -0300

    [TASK-0003] : scout code

--- src/main/java/com/io/financio/entrypoint/authenticate/AuthenticateController.java
+++ src/main/java/com/io/financio/entrypoint/authenticate/AuthenticateController.java
@@ -3,7 +3,6 @@ package com.io.financio.entrypoint.authenticate;
 import com.io.financio.domain.model.request.LoginUserRequest;
 import com.io.financio.domain.usecase.authenticate.AuthenticateUseCase;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +14,6 @@ import static com.io.financio.entrypoint.constants.RestConstants.AUTHENTICATE_UR
 @RestController
 @RequestMapping
 public class AuthenticateController {
-    public static final String LOGIN_SUCCESSFUL_FEEDBACK = "Login successful";
     private final AuthenticateUseCase useCase;
 
     public AuthenticateController(AuthenticateUseCase useCase) {
@@ -26,11 +24,10 @@ public class AuthenticateController {
     public ResponseEntity<String> login(@RequestBody LoginUserRequest request) {
         var token = useCase.execute(request);
 
-        return new ResponseEntity<>(
-                LOGIN_SUCCESSFUL_FEEDBACK,
-                buildHeader(token),
-                HttpStatus.OK
-        );
+        return ResponseEntity
+                .ok()
+                .headers(buildHeader(token))
+                .build();
     }
 
     private HttpHeaders buildHeader(String token) {
