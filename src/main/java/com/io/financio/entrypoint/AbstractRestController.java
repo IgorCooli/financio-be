18631ed07758cefb83f284580ec0fbf30fe9ef00
igorcooli <igorcooli@gmail.com>
2023-10-14 17:17:57 -0300

    [hotfix/cors] : configuring cors

--- src/main/java/com/io/financio/entrypoint/AbstractRestController.java
+++ src/main/java/com/io/financio/entrypoint/AbstractRestController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Component
 //TODO adicionar url do servidor do frontend
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://financio-fe-9626ae645fb0.herokuapp.com/"})
 public abstract class AbstractRestController {
 }
