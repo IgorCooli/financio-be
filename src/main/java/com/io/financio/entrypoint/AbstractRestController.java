98839cbde9ebb48ebe7de720d02cbebfdddcd929
igorcooli <igorcooli@gmail.com>
2023-10-14 20:07:41 -0300

    [hotfix/cors] : adjusting fe url

--- src/main/java/com/io/financio/entrypoint/AbstractRestController.java
+++ src/main/java/com/io/financio/entrypoint/AbstractRestController.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Component
 //TODO adicionar url do servidor do frontend
-@CrossOrigin(origins = {"http://localhost:3000", "https://financio-fe-9626ae645fb0.herokuapp.com/"})
+@CrossOrigin(origins = {"http://localhost:3000", "https://financio-fe-53b8b9d0c120.herokuapp.com/"})
 public abstract class AbstractRestController {
 }
